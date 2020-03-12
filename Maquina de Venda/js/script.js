75b1175cd2d4c63045afe1d36b9604665bc2aaa1
ronaldopires <ronaldo.carvalho@hotmail.com>
2020-03-12 01:08:18 -0300

    Feito a refatoração do código

--- Maquina de Venda/js/script.js
+++ Maquina de Venda/js/script.js
@@ -1,11 +1,13 @@
 let total = 0;
-let coinPenny = Number($('#Penny').val());
-let coinNickel = Number($('#Nickel').val());
-let coinDime = Number($('#Dime').val());
-let coinQuarter = Number($('#Quarter').val());
-let coinHalfDollarCoin = Number($('#HalfDollarCoin').val());
+let valor = 0;
+let coinPenny = 0.01;
+let coinNickel = 0.05;
+let coinDime = 0.10;
+let coinQuarter = 0.25;
+let coinHalfDollarCoin = 0.50;
 let coin = Number($('#coin').val(0));
 
+//Ao clicar no elemento passo o valor de coin e chamo a função calcular
 $('#Penny').on('click', function() {
     coin = coinPenny;
     Calculate();
@@ -30,18 +32,28 @@ $('#HalfDollarCoin').on('click', function() {
 
 function Calculate() {
 
-    //Caso não seja 1 ou 25 é negado
-    if ((coin != 1) && (coin != 25)) {
-        alert("Só aceitamos moedas de $0,1 e $0,25");
+    //Caso seja diferente de 0.10 ou 0.25 é negado
+    if ((coin != 0.10) && (coin != 0.25)) {
+        alert("Só aceitamos moedas de $0,10 e $0,25");
     } else {
-        coin = total + coin;
-        total = coin;
-
-        if (total >= 45) {
+        valor += coin;
+        total = valor.toFixed(2);
+        if (total >= 0.45) {
             alert("Refrigerante Liberado");
             console.log("Você gastou ", total);
-            total = 0;
-            $('#janela1').css('opacity', 0);
+            valor = 0;
+            //Esconde a janela
+            $('#door1').slideUp('slow');
+            $('#lata').on('click', function() {
+                $('#lata').hide();
+                $('#message').css('opacity', 1);
+            });
+            //Após 3 segundos mostra door1
+            setTimeout(function() {
+                $('#door1').slideDown('fast');
+                $('#lata').slideDown('slow');
+                $('#message').css('opacity', 0);
+            }, 3000);
         } else {
             console.log("Dinheiro necessário 0,45. Você tem ", total)
         }
